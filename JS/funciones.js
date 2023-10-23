636744acf5bb32d76e8aeb9fec8a4d88f1a8fd5b
PandaFuKen <118638231+PandaFuKen@users.noreply.github.com>
2023-10-22 20:09:20 -0600

    cambio de toda la pagina en general

---
+++ JS/funciones.js
@@ -0,0 +1,35 @@
+function openNav() {
+    document.getElementById("navbar").style.width = "400px";
+  }
+  
+  function closeNav() {
+    document.getElementById("navbar").style.width = "0";
+  }
+
+  var dropdown = document.getElementsByClassName("dropdown-btn");
+  var i;
+
+  for (i = 0; i < dropdown.length; i++) {
+    dropdown[i].addEventListener("click", function() {
+      this.classList.toggle("active");
+      var dropdownContent = this.nextElementSibling;
+      if (dropdownContent.style.display === "block") {
+        dropdownContent.style.display = "none";
+      } else {
+        dropdownContent.style.display = "block";
+      }
+    });
+  }
+
+  window.onscroll = function() {myFunction()};
+
+var navbar = document.getElementById("navbar");
+var sticky = navbar.offsetTop;
+
+function myFunction() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky")
+  } else {
+    navbar.classList.remove("sticky");
+  }
+}
